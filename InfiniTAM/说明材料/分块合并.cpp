283f2482fa65ec9b4841633392611901e3251214
YellowOrz <silence_33_@outlook.com>
2023-09-22 00:02:13 +0800

    perf：更新 前缀和 的说明代码

--- InfiniTAM/说明材料/分块合并.cpp
+++ InfiniTAM/说明材料/分块合并.cpp
@@ -4,20 +4,56 @@ using namespace std;
 
 int main() {
   int localSize = 256;
-  vector<int> nums(localSize, 1);
+  vector<int> prefixBuffer(localSize, 1);
   int s1, s2;
-  for (s1 = 1, s2 = 1; s1 < localSize; s1 <<= 1) {
-    for (int localId = 0; localId < localSize; localId++) {
+  //! 步骤一
+  for (s1 = 1, s2 = 1; s1 < localSize; s1 <<= 1) { // 实现一
+    for (int localId = 0; localId < localSize; localId++) {        
       s2 |= s1;
       if ((localId & s2) == s2){
-        nums[localId] += nums[localId - s1];
+        prefixBuffer[localId] += prefixBuffer[localId - s1];
         printf("localId %d\ts1 s2 %d %d\n", localId, s1, s2);
       }
     }
     printf("=====以上for循环并行发生=====\n");
   }
 
-  for(auto &n:nums)
+  // for (s1 = 1; s1 < localSize; s1 <<= 1) {       // 实现二
+  //   for (int localId = 0; localId < localSize; localId++) {
+  //     if ((localId + 1) % (s1*2) == 0) { // +1是因为localId从0开始
+  //       prefixBuffer[localId] += prefixBuffer[localId - s1];
+  //       printf("localId %d\ts1 %d\n", localId, s1);
+  //     }
+  //   }
+  //   printf("=====以上for循环并行发生=====\n");
+  // }
+
+  for(auto &n:prefixBuffer)
+    cout << n << " ";
+  cout << endl;
+  printf("###############################################################\n");
+  //! 步骤二
+  for (s1 >>= 2, s2 >>= 1; s1 >= 1; s1 >>= 1, s2 >>= 1) { // 实现一
+    for (int localId = 0; localId < localSize; localId++) {
+      if (localId != localSize - 1 && (localId & s2) == s2) {
+        prefixBuffer[localId + s1] += prefixBuffer[localId];
+        printf("localId %d\ts1 s2 %d %d\n", localId, s1, s2);
+      }
+    }
+    printf("=====以上for循环并行发生=====\n");
+  }
+
+  // for (s1 = localSize / 2; s1 >= 2; s1 /= 2) {            // 实现二
+  //   for (int localId = 0; localId < localSize; localId++) {
+  //     if (localId != localSize - 1 && (localId + 1) % s1 == 0) { // +1是因为localId从0开始
+  //       prefixBuffer[localId + s1/2] += prefixBuffer[localId];
+  //       printf("localId %d\ts1 %d\n", localId, s1);
+  //     }
+  //   }
+  //   printf("=====以上for循环并行发生=====\n");
+  // }
+
+  for(auto &n:prefixBuffer)
     cout << n << " ";
   cout << endl;
   return 0;
