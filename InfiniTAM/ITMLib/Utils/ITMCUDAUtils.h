f1ed3c2c25feabd6a3084c0393bcc184da6acc77
YellowOrz <silence_33_@outlook.com>
2023-09-21 23:52:29 +0800

    perf: 完成前缀和的注释 && 添加说明文档

--- InfiniTAM/ITMLib/Utils/ITMCUDAUtils.h
+++ InfiniTAM/ITMLib/Utils/ITMCUDAUtils.h
@@ -33,13 +33,20 @@ inline __device__ void warpReduce3(volatile float *sdata, int tid) {
   sdata[3 * tid + 2] += sdata[3 * (tid + 1) + 2];
 }
 /**
- * 计算数组的前缀和
+ * 通过计算数组的前缀和 来找到数组中想要的每个元素 在最终存放的数组中的位置
+ * @details 前缀和的计算方法：分治。
+ *          步骤一：分组求和。刚开始分组小（2个1组），计算每组之和存在最后；分组长度*2，重复计算
+ *          步骤二：更新组内中间数。从最大的分组开始，每组中间 加上前一组的最后一个数，即为它在整个数组中的前缀和。
+ *                 分组长度/2，重复计算。等长度为2的组 也计算完，整个数组的前缀和就完成了
+ *          但由于CUDA是要分grid和block。所以下面的代码先在block内部（使用共享内存）计算步骤一、二（称为block内的offset），然后计算
+ *                block之间的和，
+ *          推荐参考“说明材料/CUDA计算前缀和.pptx”和“说明材料/分块合并.cpp”
  * @tparam T 数组中数字的类型
- * @param[in] element 数组中的某个数字
- * @param[in] sum 前缀和
- * @param[in] localSize 分段大小。必须为256，∵在共享内存中申请了长256的数组
- * @param[in] localId element在所在分段中的位置
- * @return
+ * @param[in] element 数组中的某个数字。值为0（表示不要） or 1（表示想要）。    // TODO: 应该不会是其他取值了吧
+ * @param[in,out] sum 数组中最大的前缀和，即想要的元素个数
+ * @param[in] localSize cuda的block大小。必须为256，∵在共享内存中申请了长256的数组
+ * @param[in] localId block中的线程id
+ * @return 每个元素 在最终存放的数组中的位置。-1表示这个元素不需要
  */
 template<typename T>
 __device__ int computePrefixSum_device(uint element, T *sum, int localSize, int localId) {
@@ -51,28 +58,47 @@ __device__ int computePrefixSum_device(uint element, T *sum, int localSize, int 
   __syncthreads();
 
   int s1, s2;
-  for (s1 = 1, s2 = 1; s1 < localSize; s1 <<= 1) {
-    s2 |= s1;   // TODO: 下次从这儿开始。假设localId=3
-    // NOTE: A & B = B 说明 B 中所有为1的位在 A 中也必须为1
-    if ((localId & s2) == s2) prefixBuffer[localId] += prefixBuffer[localId - s1];
+  //! block内部 分组求和。s1是步长（以2的倍数增长），s2用来选择每组最后一个数（用来存放求和的值）。
+  // NOTE：以下两个for循环可以运行“说明材料/分块合并.cpp”，通过输出进行理解
+  for (s1 = 1, s2 = 1; s1 < localSize; s1 <<= 1) {    // NOTE: 可以参考“说明材料/CUDA计算前缀和.pptx”中第一页
+    s2 |= s1;   // s2从左往右一位一位变成1
+    // NOTE: A & B = B 说明 B 中所有为1的位在 A 中也必须为1。这里是找到localId的二进制中从低位有连续几个1，这个for循环就遍历几遍
+    // 例如，localId=71，二进制为1000111，最低位有3个1，则该for循环会遍历3次
+    if ((localId & s2) == s2)   // 位置是s2
+      prefixBuffer[localId] += prefixBuffer[localId - s1];
     __syncthreads();
   }
-
-  for (s1 >>= 2, s2 >>= 1; s1 >= 1; s1 >>= 1, s2 >>= 1) {
-    if (localId != localSize - 1 && (localId & s2) == s2) prefixBuffer[localId + s1] += prefixBuffer[localId];
+  /* 上面for循环改成这样更容易理解
+  for (s1 = 1; s1 < localSize; s1 *= 2) {
+    if((localId + 1) % (s1*2) == 0)   // +1是因为localId从0开始，s1*2是因为 分组长度 是 步长的2倍
+      prefixBuffer[localId] += prefixBuffer[localId - s1];
     __syncthreads();
   }
-
+  */
+  //! block内部 更新组内最中间的数。步长从64开始。得到block内部的前缀和
+  for (s1 >>= 2, s2 >>= 1; s1 >= 1; s1 >>= 1, s2 >>= 1) {   // NOTE: 可以参考“说明材料/CUDA计算前缀和.pptx”中第二页
+    if (localId != localSize - 1 && (localId & s2) == s2) 
+      prefixBuffer[localId + s1] += prefixBuffer[localId];  // 第一个循环只有prefixBuffer[192] += prefixBuffer[128]
+    __syncthreads();
+  }
+  /* 上面for循环改成这样更容易理解
+  for (s1 = localSize / 2; s1 >= 2; s1 /= 2) {
+    if(localId != localSize - 1 && (localId + 1)%s1 == 0)   // +1是因为localId从0开始
+      prefixBuffer[localId+s1/2] += prefixBuffer[localId];  // s1/2表示当前区间的中间，localId是上一个区间的最后
+    __syncthreads();
+  }
+  */
+  //! block之间 求和。得到整个数组（全局）的前缀和
   if (localId == 0 && prefixBuffer[localSize - 1] > 0) groupOffset = atomicAdd(sum, prefixBuffer[localSize - 1]);
   __syncthreads();
-
+  //! 计算当前元素在整个数组（全局）的前缀和
   int offset;// = groupOffset + prefixBuffer[localId] - 1;
   if (localId == 0) {
-    if (prefixBuffer[localId] == 0) offset = -1;
+    if (prefixBuffer[localId] == 0) offset = -1;                          // 前缀和为0，表示当前元素值为0，不想要
     else offset = groupOffset;
   } else {
-    if (prefixBuffer[localId] == prefixBuffer[localId - 1]) offset = -1;
-    else offset = groupOffset + prefixBuffer[localId - 1];
+    if (prefixBuffer[localId] == prefixBuffer[localId - 1]) offset = -1;  // 跟前一个前缀和相同，表示当前元素值为0，不想要
+    else offset = groupOffset + prefixBuffer[localId - 1];                // 当前元素在整个数组（全局）的前缀和，想要
   }
 
   return offset;
